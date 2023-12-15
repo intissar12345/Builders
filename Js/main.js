ee763f0a83b3b7c68956035b19b8ea7b2825e5a6
intissar12345 <144015814+intissar12345@users.noreply.github.com>
2023-12-15 15:31:53 +0100

    done

--- Js/main.js
+++ Js/main.js
@@ -1,3 +1,21 @@
 console.log("het werkt");
 
-let blocks = document.querySelectorAll(".box");
+let boxes = document.querySelectorAll(".box");
+let turn = "player";
+
+boxes.forEach((box) => {
+    box.addEventListener('click', function () {
+        if (turn == "player" && box.textContent == "") {
+            box.textContent = "boot player";
+            turn = "computer";if(turn=="explosie"&& box.textContent==""){
+                box.textContent="explosie";
+                turn="player"
+            }
+        } else if (turn == "computer" && box.textContent == "") {
+            box.textContent = "boot computer";
+            turn = "player";
+        };
+    });
+});
+
+
